77d1a8957663833833eb1e87d4d934e30b59d211
OsiyoComputers <hamidovasadbek09@gmail.com>
2026-03-30 12:37:36 +0500

    fix allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ from decouple import config
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['toptransfers.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
